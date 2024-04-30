eb3d552db139f7ccd58ad9a08f01d25951eccf6e
XX <ro0t@foxmail.com>
2024-04-30 15:35:36 +0800

    依赖版本冲突解决 新增：使用spleeter提取人声

--- Sperator.py
+++ Sperator.py
@@ -1,16 +1,21 @@
+import os
+
 from spleeter.separator import Separator
 from spleeter.audio.adapter import AudioAdapter
 from multiprocessing import freeze_support
 
 # python -m spleeter separate -p spleeter:2stems -o output "1672507817.mp3"
-separator = Separator('spleeter:2stems')
-input_ = r'C:\Users\SN78083\Music\搁浅.m4a'
-# audio_loader = AudioAdapter.default()
-# sample_rate = 44100
-# waveform, _ = audio_loader.load(input_, sample_rate=sample_rate)
+path = folder + '/' + file_name + '/vocals.wav'
+print(os.path.abspath(path))
 
-# Perform the separation :
-# prediction = separator.separate(waveform)
-# freeze_support()
-separator.separate_to_file(input_, 'output', synchronous=False)
+if __name__ == '__main__':
+    freeze_support()
+    separator = Separator('spleeter:2stems')
+    input_ = r'C:\Users\SN78083\Music\搁浅.m4a'
+    # audio_loader = AudioAdapter.default()
+    # sample_rate = 44100
+    # waveform, _ = audio_loader.load(input_, sample_rate=sample_rate)
 
+    # Perform the separation :
+    # prediction = separator.separate(waveform)
+    separator.separate_to_file(input_, 'output', synchronous=False)
